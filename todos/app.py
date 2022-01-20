41f9777e21c988e6be41e5a345039a21defb2e03
skyreep <skyreep@gmail.com>
2022-01-19 23:48:24 -0500

    Added ToDo item removal functionality

--- todos/app.py
+++ todos/app.py
@@ -8,18 +8,28 @@ app.config['SECRET_KEY'] = 'mysecret'
 
 todos = ["Learn Flask", "Setup venv", "Build a cool app"]
 
-class TodoForm(FlaskForm):
-    todo = StringField("Todo to Add", validators=[DataRequired()])
+class AddTodo(FlaskForm):
+    todo = StringField("Add a Todo", validators=[DataRequired()])
     add = SubmitField("Add Todo")
-    removalnum = IntegerField("Todo to Remove", validators=[DataRequired()])
+
+class RemoveTodo(FlaskForm):
+    removalnum = IntegerField("Remove a Todo", validators=[DataRequired()])
     remove = SubmitField("Remove Todo")
+
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    todo_form=TodoForm()
-    if todo_form.validate_on_submit():
-        new_todo=todo_form.todo.data
-        todos.append(new_todo)
-    #elif 'removalnum' in request.form:
-        #todos.pop('removalnum')
-    return render_template('index.html', todos=todos, template_form=todo_form)
+    #instantiate the form
+    todo_form = AddTodo()
+    remove_form = RemoveTodo()
+
+    #handle todo adding
+    if todo_form.validate_on_submit(): #form validation to prevent blank submissions
+        new_todo = todo_form.todo.data #accessing form data through the form instance
+        todos.append(new_todo) #appending form data to list
+    #handle todo removal 
+    elif remove_form.validate_on_submit():
+        if len(todos) >= remove_form.removalnum.data: #check to make sure the list item at the user's removal index exists
+            todos.pop(remove_form.removalnum.data - 1) #remove the item from the list
+
+    return render_template('index.html', todos=todos, template_form=todo_form, template_form2=remove_form)
